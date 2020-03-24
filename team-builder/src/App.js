21198c7239fe5befeab5a6b23b4dca82f8e9df38
Zoe Stokes <sekotszs@gmail.com>
2020-03-24 12:42:28 -0700

    started

---
+++ team-builder/src/App.js
@@ -0,0 +1,18 @@
+import React,{useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  const TeamMembers = [
+    {firstName: 'zoe', lastName:'stokes', role: 'furry'},
+    {firstName: 'marissa', lastName:'cooter', role: 'nonFurry'},
+    {firstName: 'christina', lastName:'harris', role: 'catLady'},
+    {firstName: 'kenzie', lastName:'morlock', role: 'cutiePie'}
+  ]
+  return (
+    
+  );
+}
+
+export default App;
